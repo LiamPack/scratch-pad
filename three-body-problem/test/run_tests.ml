800438e2df3108d694f4ca8541a961a0c963c255
Liam Packer <liampacker@gmail.com>
2021-12-24 16:16:32 -0500

    what is testing but seeing if graphs look correct. so the graphs are made in the tests

--- three-body-problem/test/run_tests.ml
+++ three-body-problem/test/run_tests.ml
@@ -1,7 +1,8 @@
 open OUnit2
+open Three_body
 
 let tests =
   "test suite three-body-problem" >::: [ ("placeholder" >:: fun _ -> assert_equal true true) ]
 
 
-let run_test = run_test_tt_main tests
+let run_test = plot 10.
