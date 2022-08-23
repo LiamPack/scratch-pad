43c51a7de13f69610a1d7507be2b6fd4135bec49
Liam <liampacker@gmail.com>
2022-08-22 23:47:15 -0400

    cute little matching problem solution, 1-way choices that's not optimal but fine enough
    mentees fix

--- student-prof-matching/main.py
+++ student-prof-matching/main.py
@@ -1,24 +1,26 @@
 import networkx as nx
 
-student_prof_preferences = {
-    'a': ['2', '3'], 'b': ['3', '4', '2']
-}
+mentees_df = pd.read_csv('anonymous_mentees.csv')
+mentees_df = mentees_df.iloc[:, 1:4] # ... tuned to header hopefully,
+                                     # should be "name" "# needed
+                                     # mentors", "choice1", "choice2",
+                                     # "choice3"
 
-students = list(set(student_prof_preferences.keys()))
-profs = list(set([p for l in student_prof_preferences.values() for p in l]))
-g = nx.DiGraph(student_prof_preferences)
+mentors_df = pd.read_csv('anonymous_menotrs.csv')
+mentors_df = mentors_df[['Name', 'number of mentees']]
 
-for s,t in g.edges():
-    if s in students:
-        g[s][t]['capacity'] = 1 
+g = nx.DiGraph()
 
+[g.add_edge(row[0], 't', capacity=row[1]) for row in zip(mentors_df['Name'], mentors_df['Maximum Number of Mentees'])]
+[g.add_edge('s', row[0], capacity=row[1]) for row in zip(mentees_df.iloc[:, 0], mentees_df.iloc[:, 1])]
 
-g.add_node('s')
-[g.add_edge('s', s, capacity=1) for s in students]
+students = list(set([t for s,t in g.edges() if s == 's']))
+profs = list(set([s for s,t in g.edges() if t == 't']))
 
-g.add_node('t')
-g.add_edge('2', 't', capacity=2)
-g.add_edge('3', 't', capacity=1)
-g.add_edge('4', 't', capacity=0)
+for s, p1, p2, p3 in zip(mentees_df.iloc[:, 0], mentees_df.iloc[:, 2], mentees_df.iloc[:, 3], mentees_df.iloc[:, 4]):
+    g.add_edge(s, p1, capacity=1)
+    g.add_edge(s, p2, capacity=1)
+    g.add_edge(s, p3, capacity=1)
 
-nx.maximum_flow(g, 's', 't')
+max_flow, edges = nx.maximum_flow(g, 's', 't')
+assignments = {k:v for k,v in edges.items() if 't' not in v.keys() and 's' != k and 't' != k and k in students}
