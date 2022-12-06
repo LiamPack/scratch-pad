b09f058e8a0d80d12b3be14d892e49b144b946aa
Liam <liampacker@gmail.com>
2022-12-06 00:26:20 -0500

    aoc day 6 quickie
    greatly simplified soln, no allocaiton
    
    p1 & p2

---
+++ advent-of-code-solutions/2022/6/6.jl
@@ -0,0 +1,12 @@
+input = open(f -> read(f, String), "input.txt")[1:end-1] # newline as usual
+
+function solve(s, N)
+    for i in 1:length(s)
+        if s[i:(i+N-1)] |> unique |> length == N
+            return i + N - 1
+        end
+    end
+end
+
+p1 = solve(input, 4)
+p2 = solve(input, 14)
