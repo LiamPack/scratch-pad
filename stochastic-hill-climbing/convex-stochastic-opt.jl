8b66b73b898eb1c8e52a2d0353a00f70306c1156
Liam Packer <liampacker@gmail.com>
2023-09-12 22:40:42 -0400

    add temperature and region tuning

--- stochastic-hill-climbing/convex-stochastic-opt.jl
+++ stochastic-hill-climbing/convex-stochastic-opt.jl
@@ -7,6 +7,9 @@ d(n) = MvNormal(zeros(n), I)
 
 unit_sphere(x) = norm(x) <= 1
 in_sphere(x, r) = unit_sphere(x/r)
+in_ellipsoid(x,Rs) = unit_sphere(x ./ Rs)
+
+in_2d_crescent(x) = (!in_sphere(x, 1)) * in_ellipsoid(x, [2, 1])
 
 unit_cube(x) = norm(x, Inf) <= 1
 in_cube(x, dims) = unit_cube(x ./ (dims ./ 2))
@@ -37,11 +40,12 @@ function torus(N, L)
     return xs
 end
 
-function ndtorus(N, L, d)
+function ndtorus(N, L, d; constraint=unit_cube, heat=(x -> x[1] > 5 ? 4 : 2))
     warmup = floor(Int, N/10)
     xs = [rand(Float64, d) .* L]
     
-    temp = make_temperature(x -> shift(x, 5) |> unit_cube, x -> 5)
+    # temp = make_temperature(x -> shift(x, 5) |> constraint, heat)
+    temp(x) = ((shift(x, 5) |> constraint) ? 0.5 : heat(x))
     for i in 1:N
         xn = xs[end]
         push!(xs, step_torus(xn, temp(xn), L))
@@ -51,12 +55,16 @@ end
 
 
 
-xs = ndtorus(1e6, 10, 10)
+xs = ndtorus(1e7, 10, 2; constraint=unit_sphere, heat=(x -> x[1] > 5 ? 4 : 2))
 xs0 = xs .|> x -> x .- 5
 rs = norm.(xs0)
 
 
-# histogram2d(first.(xs), xs .|> v -> v[2], normalize=:pdf, bins=(20,20))
+
+
+
+# histogram2d(first.(xs), xs .|> v -> v[2], normalize=:pdf, bins=(100,100))
+# stephist(rs, normalize=:pdf, bins=100)
 
 # shift back by -x0  before plotting
 # keep on torus -- only place we get ergodicity probably. need stddev <= 1/2 * L probably
