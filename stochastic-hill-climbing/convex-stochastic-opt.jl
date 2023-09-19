52ed505e91802f8ac56f928601f8462fff7ef329
Liam Packer <liampacker@gmail.com>
2023-09-19 19:52:30 -0400

    add ergodic path plotting, heat over time

--- stochastic-hill-climbing/convex-stochastic-opt.jl
+++ stochastic-hill-climbing/convex-stochastic-opt.jl
@@ -40,12 +40,13 @@ function torus(N, L)
     return xs
 end
 
-function ndtorus(N, L, d; constraint=unit_cube, heat=(x -> x[1] > 5 ? 4 : 2))
-    warmup = floor(Int, N/10)
-    xs = [rand(Float64, d) .* L]
+function ndtorus(N, L, d; init=rand(Float64, d).*L, constraint=unit_cube, heat=(x -> x[1] > 5 ? 4 : 2))
+    # warmup = floor(Int, N/10)
+    warmup=1
+    xs = [init]
     
     # temp = make_temperature(x -> shift(x, 5) |> constraint, heat)
-    temp(x) = ((shift(x, 5) |> constraint) ? 0.5 : heat(x))
+    temp(x) = ((shift(x, L/2) |> constraint) ? 0.5 : heat(x))
     for i in 1:N
         xn = xs[end]
         push!(xs, step_torus(xn, temp(xn), L))
@@ -55,13 +56,16 @@ end
 
 
 
-xs = ndtorus(1e7, 10, 2; constraint=unit_sphere, heat=(x -> x[1] > 5 ? 4 : 2))
-xs0 = xs .|> x -> x .- 5
-rs = norm.(xs0)
+Xs = [reduce(hcat, ndtorus(2000, 10, 2; constraint=unit_sphere, heat=(x -> x[1] > 5 && x[2] > 5 ? 10 : 2.0))) for _ in 1:1e6]
 
+extract_n(Xs,n) = reduce(hcat, Xs .|> v -> v[:,n])
 
-
-
+anim = @animate for i in 1:2000
+    histogram2d(extract_n(Xs, i)[1,:], extract_n(Xs,i)[2,:], normalize=:pdf, bins=(100,100))
+end
+gif(anim, "test_anim_orthant_extreme_but0-8_long.gif", fps=20)
+# xs0 = xs .|> x -> x .- 5
+# rs = norm.(xs0)
 
 # histogram2d(first.(xs), xs .|> v -> v[2], normalize=:pdf, bins=(100,100))
 # stephist(rs, normalize=:pdf, bins=100)
